7947b414cff5d62f7a3131baf0ddedca612f84cb
krloves <karen.lovedorial@gmail.com>
2025-11-15 19:52:17 +0800

    finish reverseStrings.js & reverseStrings.spec.js exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    string = "" + string;
+    let stringArr = string.split('');
+    let stringLen = stringArr.length;
+    let reversedString = "";
 
+    for(let i=0; i < stringLen; i++) {
+        reversedString += stringArr.pop();
+    }
+    return reversedString
 };
 
 // Do not edit below this line
