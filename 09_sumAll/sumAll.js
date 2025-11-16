256b5f46ee91c59e71803e4736a5ad49cafc16ab
krloves <karen.lovedorial@gmail.com>
2025-11-16 12:59:57 +0800

    finish and pass sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (0 <= a && 0 <= b) {
+            abArr = [a, b].sort((x, y) => x - y);
+            smallNum = abArr.at(0)
+            bigNum = abArr.at(1)
+            let sum = 0;
 
+            for (let i=smallNum; i <= bigNum; i++) {
+                sum += i
+            }
+            return sum
+
+        } else {
+            return 'ERROR'
+        }
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
