f8a923a1d2de6cad8ec376c276a220496aebbe57
krloves <karen.lovedorial@gmail.com>
2025-11-16 12:36:09 +0800

    add shorter solution in removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,27 +1,42 @@
+// SOLUTION # 1
+
+// const removeFromArray = function(array, ...toRemove) {
+//     let arrayLen = array.length;
+//     let toRemoveLen = toRemove.length;
+//     let returnArray = [];
+
+//     for (let i=0; i < arrayLen; i++) {
+//         let shouldRemove = false;
+
+//         for (let r=0; r < toRemoveLen; r++) {
+//             if (array.at(i) === toRemove.at(r)) {
+//                 shouldRemove = true;
+//                 break;
+//             }
+//         }
+
+//         if (shouldRemove == false) {
+//             returnArray.push(array[i])
+//         }
+//     }
+
+//     return returnArray
+
+// };
+
 const removeFromArray = function(array, ...toRemove) {
-    let arrayLen = array.length;
-    let toRemoveLen = toRemove.length;
     let returnArray = [];
 
-    for (let i=0; i < arrayLen; i++) {
-        let shouldRemove = false;
-
-        for (let r=0; r < toRemoveLen; r++) {
-            if (array.at(i) === toRemove.at(r)) {
-                shouldRemove = true;
-                break;
-            }
+    array.forEach(element => {
+        
+        if (!toRemove.includes(element)) {
+            returnArray.push(element);
         }
+    });
 
-        if (shouldRemove == false) {
-            returnArray.push(array[i])
-        }
-    }
-
-    return returnArray
-
+    return returnArray;
+    
 };
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
