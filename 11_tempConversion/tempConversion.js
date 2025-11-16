71fc507094cc6a0d4d106d6ca607c21d58e4957a
krloves <karen.lovedorial@gmail.com>
2025-11-16 13:16:24 +0800

    finish and pass tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+    if (typeof(tempFahrenheit) === 'number') {
+      let tempCelcius = (tempFahrenheit - 32) * 5/9
+      return Math.round(tempCelcius * 10) / 10
+    } else {
+      return "Input is not a valid number!"
+    }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelcius) {
+    if (typeof(tempCelcius) === 'number') {
+      let tempFahrenheit = tempCelcius * (9/5) + 32
+      return Math.round(tempFahrenheit * 10) / 10
+    } else {
+      return "Input is not a valid number!"
+    }
 };
 
 // Do not edit below this line
