00acc451591f078716cf8b39e4c357d9d3ec7cfe
krloves <karen.lovedorial@gmail.com>
2025-11-16 14:52:00 +0800

    finish and pass palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    cleanStrLen = cleanStr.length;
+    cleanStrArr = cleanStr.split('');
+    let midIndex;
+
+
+    if (cleanStrLen % 2  == 0) {
+        midIndex = (cleanStrLen / 2) - 1
+    } else {
+        midIndex =((cleanStrLen - 1) / 2) - 1
+    }
+
+    for (let i=0; i <= midIndex; i++) {
+        let firstChar = cleanStrArr.shift();
+        let lastChar = cleanStrArr.pop();
+
+        if (firstChar != lastChar) {
+            // return "" + firstChar != lastChar + i + firstChar + lastChar + cleanStrArr;
+            return false
+        } 
+    }
+
+    return true
 
 };
 
