8d05bb8353ed2ddf164346fa81a75e4a442e502d
krloves <karen.lovedorial@gmail.com>
2025-11-16 13:49:03 +0800

    finish and pass calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let argsSum = 0;
+
+  args.forEach(element => {
+      argsSum += element
+    }
+  );
+
+	return argsSum;
 };
 
-const multiply = function() {
 
+const multiply = function(args) {
+  let argsProduct = 1;
+
+  args.forEach(element => {
+      argsProduct *= element
+    }
+  );
+
+	return argsProduct;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let b = 1;
+  while (a > 0) {
+    b *= a;
+    a--;
+  }
+  return b
 };
 
 // Do not edit below this line
