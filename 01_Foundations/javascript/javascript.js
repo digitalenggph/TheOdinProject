60a2118704f788358ce54eb566b525b1549fcaf6
krloves <karen.lovedorial@gmail.com>
2025-11-23 16:01:28 +0800

    add playGame function logic

--- 01_Foundations/javascript/javascript.js
+++ 01_Foundations/javascript/javascript.js
@@ -1,9 +1,12 @@
 const elementsArr = ["rock", "paper", "scissors"];
-let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
+    console.log(`Computer chose ${elementsArr.at(randomIndex)}`);
     return elementsArr.at(randomIndex);
 };
 
@@ -12,6 +15,7 @@ function getHumanChoice() {
     let lowercaseHumanChoice = rawHumanChoice.toLocaleLowerCase();
 
     if (elementsArr.includes(lowercaseHumanChoice)) {
+        console.log(`You chose ${lowercaseHumanChoice}.`);
         return lowercaseHumanChoice;
     } else {
         console.log("No! You must choose from rocks, paper, scissors only.");
@@ -21,13 +25,28 @@ function getHumanChoice() {
 
 function playRound(human, computer) {
     if (human == computer) {
-        console.log("Tie")
+        console.log("Tie");
     } else if (human == "rock" && computer == "scissors" || 
                human == "paper" && computer == "rock" ||
-               human == "scissors" && computer == "rock"){
-        console.log("Human player wins!")
+               human == "scissors" && computer == "rock") {
+        console.log("Human player wins!");
+        humanScore += 1;
     } else {
-        console.log("Computer wins")
+        console.log("Computer wins");
+        computerScore += 1;
     }
-}
+};
 
+function playGame() {
+    for(let round = 1; round <= 5; round++) {
+        playRound();
+    };
+
+    if (humanScore == computerScore) {
+        console.log("You are tie with the computer.")
+    } else if (humanScore > computerScore) {
+        console.log("The ultimate winner is Player Human!")
+    } else {
+        console.log("The ultimate winner is Player Computer!")
+    }
+};
