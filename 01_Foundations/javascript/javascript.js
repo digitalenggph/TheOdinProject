5642b74838cee5593b2c95030f05555180685808
krloves <karen.lovedorial@gmail.com>
2025-11-23 15:23:45 +0800

    add getHumanChoice function

--- 01_Foundations/javascript/javascript.js
+++ 01_Foundations/javascript/javascript.js
@@ -1,6 +1,18 @@
-elementsArr = ["rock", "paper", "scissors"];
+const elementsArr = ["rock", "paper", "scissors"];
 
 function getComputerChoice () {
     let randomIndex = Math.floor(Math.random() * 3);
     return elementsArr.at(randomIndex);
+};
+
+function getHumanChoice() {
+    let rawHumanChoice = prompt("Choose: rock, paper, scissors", getComputerChoice());
+    let humanChoice = rawHumanChoice.toLocaleLowerCase();
+    
+    if (elementsArr.includes(rawHumanChoice.toLowerCase())) {
+        return humanChoice;
+    } else {
+        console.log("No! You must choose from rocks, paper, scissors only.");
+        getHumanChoice();
+    }
 }
