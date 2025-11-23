fe37c0c19bb318edb181c89d0cd57bb198dcc4c0
krloves <karen.lovedorial@gmail.com>
2025-11-23 15:28:18 +0800

    add const variable for getHumanChoice and getComputerChoice function

--- 01_Foundations/javascript/javascript.js
+++ 01_Foundations/javascript/javascript.js
@@ -1,18 +1,18 @@
 const elementsArr = ["rock", "paper", "scissors"];
 
-function getComputerChoice () {
+const computerChoice = function getComputerChoice () {
     let randomIndex = Math.floor(Math.random() * 3);
     return elementsArr.at(randomIndex);
 };
 
-function getHumanChoice() {
+const humanChoice = function getHumanChoice() {
     let rawHumanChoice = prompt("Choose: rock, paper, scissors", getComputerChoice());
-    let humanChoice = rawHumanChoice.toLocaleLowerCase();
-    
-    if (elementsArr.includes(rawHumanChoice.toLowerCase())) {
-        return humanChoice;
+    let lowercaseHumanChoice = rawHumanChoice.toLocaleLowerCase();
+
+    if (elementsArr.includes(lowercaseHumanChoice)) {
+        return lowercaseHumanChoice;
     } else {
         console.log("No! You must choose from rocks, paper, scissors only.");
         getHumanChoice();
     }
-}
+};
