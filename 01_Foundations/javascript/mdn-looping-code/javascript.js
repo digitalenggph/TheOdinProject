b79dc2e7caef497675f236bcc44063f60c046fa5
krloves <karen.lovedorial@gmail.com>
2025-11-25 14:50:22 +0800

    finish countdown exercise

--- 01_Foundations/javascript/mdn-looping-code/javascript.js
+++ 01_Foundations/javascript/mdn-looping-code/javascript.js
@@ -1,38 +1,25 @@
-/*
+const output = document.querySelector(".output");
+const heading = document.querySelector(".heading")
+const btn = document.querySelector("button");
+const ONESECOND = 1000
 
-const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+output.textContent = "";
 
-for (const cat of cats) {
-  console.log(cat);
-}
-
-
-
-function toUpper(string) {
-  return string.toUpperCase();
-}
-
-const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
-
-const upperCats = cats.map(toUpper);
-
-console.log(upperCats);
-// [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
-
-
-
-function lCat(cat) {
-  return cat.startsWith("L");
-}
-
-const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
-const filtered = cats.filter(lCat);
-console.log(filtered);
-// [ "Leopard", "Lion" ]
-
-*/
-
-const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
-const filtered = cats.filter(cat => cat.startsWith("L"));
-console.log(filtered);
-// [ "Leopard", "Lion" ]
+btn.addEventListener("click", () => {
+    for (let i = 10; i >= 0; i--) {
+         const para = document.createElement("p");
+        
+        setTimeout(function() { 
+            if (i == 10) {
+                para.textContent = "Countdown 10";
+            } else if (i == 0) {
+                para.textContent = "Blast off!";
+            } else {
+                para.textContent = i;
+            }
+            output.appendChild(para);
+        }, (10 - i) * ONESECOND);
+                    
+         
+    }
+});
