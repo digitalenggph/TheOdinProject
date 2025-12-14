91585470ae04f1949ad811f7b0a579ed03c731a9
krloves <karen.lovedorial@gmail.com>
2025-12-14 13:36:48 +0800

    finish UI-based logic

--- 01_Foundations/javascript/rock-paper-scissors/javascript.js
+++ 01_Foundations/javascript/rock-paper-scissors/javascript.js
@@ -1,74 +1,83 @@
 const buttonChoices = document.querySelector('#button-choices');
-const buttons = document.querySelectorAll('#button-choices button')
-const humanScoreText = document.querySelector("#human-score")
-const computerScoreText = document.querySelector("#computer-score")
+const buttons = document.querySelectorAll('#button-choices button');
+const buttonReset = document.querySelector('#reset');
+const humanScoreText = document.querySelector("#human-score");
+const computerScoreText = document.querySelector("#computer-score");
+const scoreboard = document.querySelector('h3');
+const elementsArr = ["rock", "paper", "scissors"];
 
-let elementsArr = ["rock", "paper", "scissors"]
 let computerScore = 0;
 let humanScore = 0;
 
-buttonChoices.addEventListener('click', (e) => {
-    let target = e.target;
-    let humanChoice = target.id.replace("btn-", "");
-    let computerChoice = getComputerChoice();
+buttonChoices.addEventListener('click', playGame);
+buttonReset.addEventListener('click', reset);
 
-    if (humanScore !== 5 && computerScore !==5) {
-        playRound(humanChoice, computerChoice);
-        humanScoreText.textContent = humanScore;
-        computerScoreText.textContent = computerScore;
-    } else {
-        for (const button of buttons) {
-            button.disabled = true;
-        }
-    };
-});
+
+function playGame(e) {
+    if (!e.target.matches('button')) return;   // do not proceed logic if item clicked is not button
+
+    const target = e.target;
+    const humanChoice = target.id.replace("btn-", "");
+    const computerChoice = getComputerChoice();
+    const winner = playRound(humanChoice, computerChoice);
+
+    if (winner === "human") humanScore++;
+    if (winner === "computer") computerScore++;
+    updateScoreboard();
+
+    if (humanScore === 5 || computerScore ===5) {
+        disableButtons();
+        announceWinner();
+    };   
+}
 
 function getComputerChoice() {
-    let randomIndex = Math.floor(Math.random() * 3);
-    console.log(`Computer chose ${elementsArr.at(randomIndex)}`);
-    return elementsArr.at(randomIndex);
+    const randomIndex = Math.floor(Math.random() * 3);
+    return elementsArr[randomIndex];
 };
 
-
-
 function playRound(human, computer) {
-    if (human == computer) {
-        console.log("Tie");
-    } else if (human == "rock" && computer == "scissors" || 
-               human == "paper" && computer == "rock" ||
-               human == "scissors" && computer == "rock") {
-        console.log("Human player wins!");
-        humanScore += 1;
+    if (human === computer) {
+        return "tie"
+    } else if (
+        (human === "rock" && computer === "scissors") || 
+        (human === "paper" && computer === "rock" )||
+        (human === "scissors" && computer === "paper") 
+    ) {
+        return "human";
     } else {
-        console.log("Computer wins");
-        computerScore += 1;
+        return "computer";
     }
 };
 
-// function playGame() {
+function updateScoreboard() {
+    humanScoreText.textContent = humanScore;
+    computerScoreText.textContent = computerScore;
+};
 
+function disableButtons(){
+    buttons.forEach(button => button.disabled = true);
+};
 
-//     while (humanScore !== 5 && computerScore !==5) {
-//         let humanChoice = getHumanChoice();
-//         let computerChoice = getComputerChoice();
-//         playRound(humanChoice, computerChoice);
+function enableButtons(){
+    buttons.forEach(button => button.disabled = false);
+};
 
+function announceWinner() {
+    const winner = (humanScore > computerScore)
+        ? "Human wins!"
+        : "Computer wins!"
 
+    const winnerSpan = document.createElement("span");
+    winnerSpan.textContent = winner;
+    scoreboard.appendChild(winnerSpan);
+};
 
+function reset() {
+    computerScore = 0;
+    humanScore = 0;
+    updateScoreboard();
+    enableButtons();
+    scoreboard.textContent = "Scoreboard "
+};
 
-//         console.log(`Human: ${humanScore} - Computer: ${computerScore}`);
-//     }
-
-// };
-
-function announceWinner(humanScore, computerScore) {
-    if (humanScore == computerScore) {
-        console.log("You are tie with the computer.");
-    } else if (humanScore > computerScore) {
-        console.log("The ultimate winner is Player Human!");
-    } else {
-        console.log("The ultimate winner is Player Computer!");
-    };
-
-    console.log(`Human: ${humanScore} - Computer: ${computerScore}`);
-}
