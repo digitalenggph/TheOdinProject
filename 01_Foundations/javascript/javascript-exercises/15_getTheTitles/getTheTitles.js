387d2bfdf0732a74c068d8959e7fcfe7b89736c7
krloves <karen.lovedorial@gmail.com>
2026-01-03 22:14:53 +0800

    finish getTheTitles exercise

--- 01_Foundations/javascript/javascript-exercises/15_getTheTitles/getTheTitles.js
+++ 01_Foundations/javascript/javascript-exercises/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    // Option 1
+    // let finalArr = [];
+    // arr.forEach(currentElement => {
+    //     finalArr.push(currentElement.title);
+    // });
 
+    // return finalArr;
+
+    return arr.map((element) => element.title);
 };
 
 // Do not edit below this line
