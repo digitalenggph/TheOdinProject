b3a213694fd46a9a9ddbb16b023247fe9980e119
krloves <karen.lovedorial@gmail.com>
2026-01-03 22:01:10 +0800

    finish fibonacci exercise

--- 01_Foundations/javascript/javascript-exercises/14_fibonacci/fibonacci.js
+++ 01_Foundations/javascript/javascript-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (+number < 0) {
+        return "OOPS"
+    }
 
+    let a = 0;
+    let b = 1;
+    let sequenceArray = [];
+
+    for(let i=0; i<=number; i++) {
+        sequenceArray.push(a)
+        a = b;
+        b = sequenceArray.at(-1) + b;
+    }
+
+    return sequenceArray.pop()
 };
 
 // Do not edit below this line
