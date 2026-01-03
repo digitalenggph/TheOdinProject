73ebd6be0527a80f604f108c840978b0ef3fc8ad
krloves <karen.lovedorial@gmail.com>
2026-01-03 22:56:25 +0800

    finish findTheOldest exercise

--- 01_Foundations/javascript/javascript-exercises/16_findTheOldest/findTheOldest.js
+++ 01_Foundations/javascript/javascript-exercises/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objectArray) {
+    objectArray.forEach(element => {
+        if (element.yearOfDeath == undefined) {
+            element.yearOfDeath = new Date().getFullYear();
+        }});
+        
+    objectArray.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth))
+    return objectArray.pop()
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
